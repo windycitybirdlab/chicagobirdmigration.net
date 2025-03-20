a75dcd0a3ac6fbcab245f8bc27335d450f942644
J'orge <jgar0307@gmail.com>
2025-03-20 12:24:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Load the header on all pages
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("pages//header.html")
+  fetch("pages/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
